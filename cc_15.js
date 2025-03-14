948834cab0eddd086442c8e90c0aa91b6cf8acc7
AustinNorris5 <ajnorris@usf.edu>
2025-03-14 16:24:26 -0400

    Task 2: Adding Risk Items Dynamically

--- cc_15.js
+++ cc_15.js
@@ -5,3 +5,20 @@ const riskDashboard = document.getElementById("riskDashboard");
 
 //Print "Risk Dashboard Loaded" to the console
 console.log("Risk Dashboard Loaded");
+
+//Task 2: Adding Risk Items Dynamically
+
+//Write a function addRiskItem(riskName, riskLevel, department)
+function addRiskItem(riskName, riskLevel, department) {
+    const riskCard = document.createElement("div"); //Creates a new risk card (div)
+    riskCard.classList.add("riskCard"); //Assigns a class "riskCard" to each card
+   
+   
+    
+    riskDashboard.appendChild(riskCard);
+};
+
+//Test Case
+addRiskItem("Data Breach", "High", "IT");
+addRiskItem("Supply Chain Disruption", "Medium", "Operations");
+
