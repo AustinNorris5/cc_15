223b5b5358fbffb890820f7bb2e6cf7464c46597
AustinNorris5 <ajnorris@usf.edu>
2025-03-14 15:59:40 -0400

    Task 1 - Base Structure Setup

---
+++ cc_15.js
@@ -0,0 +1,7 @@
+//Task 1: Creating the Base Structure
+
+//Select the riskDashboard container
+const riskDashboard = document.getElementById("riskDashboard");
+
+//Print "Risk Dashboard Loaded" to the console
+console.log("Risk Dashboard Loaded");
