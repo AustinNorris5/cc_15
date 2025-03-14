e61039f2978db7d385c174e9ab52339baa895054
AustinNorris5 <ajnorris@usf.edu>
2025-03-14 16:56:07 -0400

    Task 3 - Removing Risk Items

--- cc_15.js
+++ cc_15.js
@@ -2,6 +2,11 @@
 
 //Select the riskDashboard container
 const riskDashboard = document.getElementById("riskDashboard");
+const riskInput = document.getElementById("riskInput");
+const riskLevel = document.getElementById("riskLevel");
+const departmentInput = document.getElementById("department");
+const riskList = document.getElementById("riskList");
+
 
 //Print "Risk Dashboard Loaded" to the console
 console.log("Risk Dashboard Loaded");
@@ -12,13 +17,9 @@ console.log("Risk Dashboard Loaded");
 function addRiskItem(riskName, riskLevel, department) {
     const riskCard = document.createElement("div"); //Creates a new risk card (div)
     riskCard.classList.add("riskCard"); //Assigns a class "riskCard" to each card
-   
-   
-    
-    riskDashboard.appendChild(riskCard);
+   riskDashboard.appendChild(riskCard);
 };
 
 //Test Case
 addRiskItem("Data Breach", "High", "IT");
 addRiskItem("Supply Chain Disruption", "Medium", "Operations");
-
